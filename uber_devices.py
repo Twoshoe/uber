b9c30bd1d2b0660375d987208fa9a265d2096987
twoshoe <twoshoe@gmail.com>
2018-05-31 16:08:50 -0400

    Uber

--- uber_devices.py
+++ uber_devices.py
@@ -5,8 +5,8 @@ import requests
 from config import CONFIG
 
 #pulls creds from ~/.uber.ini
-USERNAME = CONFIG.get('uber', 'uber_cuser')
-API_TOKEN = CONFIG.get('uber','uber_ctoken')
+USERNAME = CONFIG.get('UBER', 'uber_cuser')
+API_TOKEN = CONFIG.get('UBER','uber_ctoken')
 
 #for initial login
 HEADERS = {
@@ -15,29 +15,8 @@ HEADERS = {
         }
 
 #paremters for access_token gen
-PARAMETERS = {
-        'username': USERNAME,
-        'token': API_TOKEN,
-        }
-
-#def main():
-#    '''Logs into uber and adds device information (stored as json) to an account'''
-#    #post header + parameter to receive json data with bearer access_token
-#    auth_response = requests.post(API_URL, headers=HEADERS, data=PARAMETERS)
-
-#    #Read Device info from file
-#    with open(FILE_NAME) as json_data:
-#        device_manager = json.load(json_data)
-#        print(json.dumps(device_manager, sort_keys=True, indent=4))
-#        #header for posting device infomation
-#        device_header = {
-#                'accept': 'accept': 'application/json',
-#                'content-type': 'application/json',
-#                'authorization': 'Bearer {0}'.format(token)
-#                }
-#        device_response = requests.post(DEVICE_URL, json=device_manger,
-#                headers=device_header)
-
-#        print(device_responce)
-
+def setUp(self):
+    self.client - UbersmithClient(cfg['url'],
+            cfg['username'],
+            cfg['api_token'])
 
